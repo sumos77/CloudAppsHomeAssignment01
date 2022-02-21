eee2ef4726e6dd711e863881486e310be5718337
Martin <48153829+sumos77@users.noreply.github.com>
2022-02-21 22:07:17 +0100

    Initial commit

---
+++ github-actions-demo-app-tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace github_actions_demo_app_tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
